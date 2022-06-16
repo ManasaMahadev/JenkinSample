a0899fc23060dede1f64265b2ac7b7d79b3953d7
Manasa <Manasa@Dell>
2022-06-16 16:30:13 +0530

    Jenkin commit

---
+++ src/test/java/package1/Sample1.java
@@ -0,0 +1,27 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	
+	@Test
+	public void meth1()
+	{
+		System.out.println("This is method1");
+	}
+	
+	@Test
+	public void meth2()
+	{
+		System.out.println("This is method2");
+	}
+	
+	@Test
+	public void meth3()
+	{
+		System.out.println("This is method3");
+	}
+	
+	
+
+}
---
+++ src/test/java/package1/Sample2.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+	
+	@Test
+	public void hi()
+	{
+		System.out.println("hi");
+	}
+
+}
